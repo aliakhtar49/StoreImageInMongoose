b8475071fdcc8ebc6b64b31418edd598aec660d0
aliakhter49 <smartali.7778@yahoo.com>
2013-12-07 15:55:28 +0500

    Project

---
+++ StoreImageinMongodb/public/javascripts/ali.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: dell
+ * Date: 12/7/13
+ * Time: 1:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$('document').ready(function(){
+
+});
